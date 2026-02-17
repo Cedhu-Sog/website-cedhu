4734b8a041dbf0c8762ef6cc72be452be93c35f0
Edwin Rojas <edwin1612palacios@gmail.com>
2026-02-17 09:41:39 -0500

    Merge de la rama main

--- config/settings.py
+++ config/settings.py
@@ -12,10 +12,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SEGURIDAD
 # ===============================
 
-SECRET_KEY = os.environ.get(
-    'SECRET_KEY',
-    'django-insecure-clave-temporal-para-desarrollo'
-)
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # En desarrollo siempre True
 DEBUG = os.environ.get("DEBUG", "True") == "True"
