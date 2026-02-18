de1f193bdf0c7309ad6ec800be230e2c66c64356
Alexis <patarroyoalexis@gmail.com>
2026-02-18 08:47:54 -0500

    Revert "Merge de la rama main"
    This reverts commit 4734b8a041dbf0c8762ef6cc72be452be93c35f0.

--- config/settings.py
+++ config/settings.py
@@ -12,7 +12,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SEGURIDAD
 # ===============================
 
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = os.environ.get(
+    'SECRET_KEY',
+    'django-insecure-clave-temporal-para-desarrollo'
+)
 
 # En desarrollo siempre True
 DEBUG = os.environ.get("DEBUG", "True") == "True"
