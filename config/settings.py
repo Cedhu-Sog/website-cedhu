c25976cdb77f2694844e85e327cd27daf3a9128e
Alexis <patarroyoalexis@gmail.com>
2026-02-24 14:07:58 -0500

    fix: secret key

--- config/settings.py
+++ config/settings.py
@@ -13,8 +13,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # ===============================
 
 SECRET_KEY = os.environ.get(
-    'SECRET_KEY',
-    'django-insecure-clave-temporal-para-desarrollo'
+    'SECRET_KEY'
 )
 
 # En desarrollo siempre True
