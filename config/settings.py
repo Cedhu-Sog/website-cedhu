edc7c1171716fbaeb2983534714a85e576a9f03d
Alexis <patarroyoalexis@gmail.com>
2026-02-25 08:46:39 -0500

    Allowed host updated

--- config/settings.py
+++ config/settings.py
@@ -14,13 +14,7 @@ SECRET_KEY = os.environ.get(
 
 DEBUG = os.environ.get("DEBUG", "True") == "True"
 
-ALLOWED_HOSTS = [
-    'www.cedhu.edu.co',
-    'cedhu.edu.co',
-    'website-cedhu.onrender.com',
-    '127.0.0.1',
-    'localhost'
-]
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(",")
 
 CSRF_TRUSTED_ORIGINS = [
     'https://website-cedhu-production.up.railway.app',
